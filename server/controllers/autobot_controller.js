f0efd54c29802be23f1c83c456e386807c14c280
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-08-14 12:43:12 -0400

    example of api created

--- server/controllers/autobot_controller.js
+++ server/controllers/autobot_controller.js
@@ -2,25 +2,79 @@ const { Autobot, User } = require('../models');
 
 module.exports = {
     async getAllAutobots(req, res) {
-        const autobots = await Autobot.find({});
+        const autobots = await Autobot.find().populate({
+            path: 'createdBy',
+            select: 'email -_id'
+            //space separate the diff field you want & use the - to take away values your dont want - must sure its all in a string
+        });
         res.json(autobots);
     },
 
+    async getSingleUser(req, res) {
+        const user = await User.findById(req.user_id).populate('autobots');
+
+        res.json(user);
+    },
+
     async createAutobot(req, res) {
-        const user = await User.findById(req.user_id);
 
         const newAutobot = await Autobot.create({
             name: req.body.name,
-            color: req.body.color
+            color: req.body.color,
+            createdBy: req.user._id
         });
 
-        user.autobots.push(newAutobot._id);
-        await user.save();
+        req.user.autobots.push(newAutobot._id);
+        await req.user.save();
 
         res.json({
             message: 'success!',
             autobot: newAutobot
         });
+    },
+
+    //update
+    async updateAutobot(req, res) {
+        const autobot_id = req.body.autobot_id;
+
+        if (!req.user.autobots.includes(autobot_id)) {
+            return res.status(403).json({
+                message: 'You cannot update an autobot you did not create'
+            })
+        }
+
+        const updatedAutobot = await Autobot.findOneAndUpdate({
+            _id: autobot_id
+        }, req.body, {
+            new: true
+        });
+
+        res.json({
+            message: 'autobot updated',
+            autobot: updatedAutobot
+        })
+    },
+
+    //delete an autobot only if the user was the one to create it
+    async deleteAutobot(req, res) {
+        const autobot_id = req.body.autobot_id;
+
+        if (!req.user.autobots.includes(autobot_id)) {
+            return res.status(403).json({
+                message: 'You do not have permission to delete this autobot'
+            })
+        }
+
+        await Autobot.deleteOne({
+            _id: autobot_id
+        });
+
+        req.user.autobots.pull(autobot_id);
+        await req.user.save
+
+        res.json({
+            message: 'Autobot deleted'
+        })
     }
 
 }
--- server/routes/api_routes.js
+++ server/routes/api_routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { verify } = require('jsonwebtoken');
 
-const autobot_controller = require('../controllers/autobot_controller')
+const autobot_controller = require('../controllers/autobot_controller');
+const { User } = require('../models');
 
 
-async function blockGuestsAndAttachToken(req, res, next) {
+async function blockGuestsAndAttachUserId(req, res, next) {
     const token = req.cookies.token;
 
     if (!token) {
@@ -29,10 +30,27 @@ async function blockGuestsAndAttachToken(req, res, next) {
     }
 }
 
+async function attachUser(req, res, next) {
+    const user = await User.findById(req.user_id);
+
+    req.user = user;
+
+    next();
+}
+
 //GET route to retrieve all autobots
 router.get('/autobots', autobot_controller.getAllAutobots);
 
+//GET route to retrieve a single user & associated autobot list
+router.get('/user', blockGuestsAndAttachUserId, autobot_controller.getSingleUser)
+
 //POST route to add an autobot
-router.post('/autobots', blockGuestsAndAttachToken, autobot_controller.createAutobot);
+router.post('/autobots', blockGuestsAndAttachUserId, attachUser, autobot_controller.createAutobot);
+
+//UPDATE - PUT route to update an autobot
+router.put('/autobot', blockGuestsAndAttachUserId, attachUser, autobot_controller.updateAutobot)
+
+//DELETE route to delete an autobot(only if user made it)
+router.delete('/autobot', blockGuestsAndAttachUserId, attachUser, autobot_controller.deleteAutobot)
 
 module.exports = router;
